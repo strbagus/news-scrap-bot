f0b85e7b295b03eb81890cd9c62683d9e1ba71a6
Satrio Bagus <satrio.baguswbw@gmail.com>
2025-10-16 00:55:33 +0700

    refactor: command sub & unsub use mongodb

--- utils/menu_utils.go
+++ utils/menu_utils.go
@@ -3,75 +3,59 @@ package utils
 import (
 	"context"
 	"fmt"
-	"gobot/models"
+	"gobot/db"
+	m "gobot/models"
 	"log"
+	"time"
 
 	"github.com/go-telegram/bot"
 	tbmodels "github.com/go-telegram/bot/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func MenuSubscribe(ctx context.Context, b *bot.Bot, update *tbmodels.Update) {
-	users, _ := ReadFile[models.User]("db/users.json")
+	coll := db.Client.Database("ftibot").Collection("users")
 	chat := update.Message.Chat
-	var isExists bool
-	for _, user := range users {
-		if user.ChatID == chat.ID {
-			isExists = true
-			break
+
+	user := m.User{
+		ChatID:   chat.ID,
+		Username: chat.Username,
+	}
+	_, err := coll.InsertOne(context.TODO(), user)
+	message := "Subscribed!"
+	if err != nil {
+		message = "Subsribe failed."
+		if mongo.IsDuplicateKeyError(err) {
+			message = "You already subscribed."
 		}
 	}
-	var message string
-	if isExists {
-		message = "You already subscribed."
-	} else {
-		message = "You have subscribed!"
-		newUser := models.User{
-			ChatID:   chat.ID,
-			Username: chat.Username,
-		}
-		users = append(users, newUser)
-		WriteFile("db/users.json", users)
-	}
-	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+	_, err = b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: chat.ID,
 		Text:   message,
 	})
 	if err != nil {
-		log.Printf("Error sending subscribe message: %v", err)
+		log.Printf("ERROR: Send Message Failed: %v", err)
 	}
 }
 
 func MenuUnsubscribe(ctx context.Context, b *bot.Bot, update *tbmodels.Update) {
-	users, _ := ReadFile[models.User]("db/users.json")
-	chat := update.Message.Chat
-	var isExists bool
-	for _, user := range users {
-		if user.ChatID == chat.ID {
-			isExists = true
-			break
-		}
-	}
-	var message string
-	if !isExists {
-		message = "You not subscribed yet."
-	} else {
-		message = "You have unsubscribed!"
-		var filtered []models.User
-		for _, user := range users {
-			if user.ChatID != chat.ID {
-				filtered = append(filtered, user)
-			}
-		}
+	ctxTimeout, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
 
-		var result []models.User
-		if filtered == nil {
-			result = []models.User{}
-		} else {
-			result = filtered
-		}
-		WriteFile("db/users.json", result)
+	coll := db.Client.Database("ftibot").Collection("users")
+	chat := update.Message.Chat
+	filter := bson.M{"chatid": chat.ID}
+	message := "Unsubscribe from news."
+	res, err := coll.DeleteOne(ctxTimeout, filter)
+	if err != nil {
+		log.Printf("delete gone wrong: %v", err)
+		message = "Unsubscribe failed."
 	}
-	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+	if res.DeletedCount == 0 {
+		message = "You not subscribed yet."
+	}
+	_, err = b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: chat.ID,
 		Text:   message,
 	})
@@ -81,7 +65,7 @@ func MenuUnsubscribe(ctx context.Context, b *bot.Bot, update *tbmodels.Update) {
 }
 
 func MenuLastInfo(ctx context.Context, b *bot.Bot, update *tbmodels.Update) {
-	news, _ := ReadFile[models.NewsType]("db/news.json")
+	news, _ := ReadFile[m.NewsType]("db/news.json")
 	message := fmt.Sprintf("%s\n%s", news[0].Title, news[0].Link)
 	chatID := update.Message.Chat.ID
 	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
